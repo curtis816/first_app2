f8039ec7f897c388170c2e1555f1e8dfd293bd8b
Curtis <curtis816@gmail.com>
2015-08-02 23:02:55 +0800

    CRUD almost done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
   end
 
   def show
+      @group = Group.find(params[:id])
   end
 
   def new
@@ -12,19 +13,36 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
+      @group = Group.find(params[:id])
   end
 
   def create
+      @group = Group.create(group_params)
 
+      if @group.save
+        redirect_to groups_path, notice: '新增討論版成功'
+      else
+        render :new
+      end
   end
 
   def update
+      @group = Group.find(params[:id])
 
+      if @group.update(group_params)
+        redirect_to groups_path, notice: '修改討論版成功'
+      else
+        render :edit
+      end
   end
 
   def destroy
 
+  end
+
+  private 
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 
 end
