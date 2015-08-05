e61af21cef1b2acc0a0649658650274d1984d6be
Curtis <curtis816@gmail.com>
2015-08-06 00:41:15 +0800

    add user control

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :group
+  belongs_to :author, class_name: "User", foreign_key: :user_id
+ 
+  def editable_by?(user)
+    user && user == author
+  end
+  
   validates :content, presence: {message: "請輸入文章內容！"}
 end
