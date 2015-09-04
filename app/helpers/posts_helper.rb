03a9d10e76dfbd19807ed9c9def6939264dc5524
Curtis <curtis816@gmail.com>
2015-09-05 01:08:22 +0800

    add partial

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,6 @@
 module PostsHelper
   def render_post_content(post)
     simple_format(truncate(post.content, length: 100))
-    #post2 = params[:post].to_s
-    #post2 = post2[0..10]+'...' if post2.length > 10
+
   end
 end
